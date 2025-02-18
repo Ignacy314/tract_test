593f39191f4dd508015cd781ff4c21bd0877b882
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-02-18 15:37:14 +0100

    x

--- src/main.rs
+++ src/main.rs
@@ -46,16 +46,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let model = model.into_optimized()?;
     let model = model.into_runnable()?;
 
-    //let model = tract_onnx::onnx()
-    //    // load the model
-    //    .model_for_path(args.model)?
-    //    // specify input type and shape
-    //    .with_input_fact(0, f64::fact([4097]).into())?
-    //    // optimize the model
-    //    .into_optimized()?
-    //    // make the model runnable and fix its inputs and outputs
-    //    .into_runnable()?;
-
     let writer = BufWriter::new(File::create(args.output_file).unwrap());
 
     let mut reader = hound::WavReader::open(args.input_file).unwrap();
@@ -73,12 +63,6 @@ fn main() -> Result<(), Box<dyn Error>> {
             let input: Tensor = tract_ndarray::Array1::from_vec(scaled).into();
             let result = model.run(tvec!(input.into()))?;
             println!("{f}: result: {:?}", result[0].to_array_view::<TDim>());
-            //let best = result[0]
-            //    .to_array_view::<f32>()?
-            //    .iter()
-            //    .copied()
-            //    .zip(1..)
-            //    .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
 
             data.push(col);
             f += 1;
