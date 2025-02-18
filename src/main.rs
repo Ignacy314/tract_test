b4e6618da345d4a3a161bcdc837e6abc8b9782ef
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-02-18 12:17:40 +0100

    x

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use std::fs::File;
 use std::io::{BufWriter, Write};
 
 use clap::Parser;
+use serde::Serialize;
 
-use self::spectrogram::{HOP_LENGTH, N_FFT, STFT};
+use self::spectrogram::{HOP_LENGTH, N_FFT, Stft};
 
 mod spectrogram;
 
@@ -22,17 +23,19 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let mut writer = BufWriter::new(File::create(args.output_file).unwrap());
+    let writer = BufWriter::new(File::create(args.output_file).unwrap());
 
     let mut reader = hound::WavReader::open(args.input_file).unwrap();
     reader.seek(args.start_sample).unwrap();
 
-    let mut stft = STFT::new(N_FFT, HOP_LENGTH);
+    let mut stft = Stft::new(N_FFT, HOP_LENGTH);
     let samples = reader.samples::<i32>();
     let mut buf = [0f64; 4096];
     let mut j = 0;
     let mut f = 0;
-    write!(writer, "[").unwrap();
+    //write!(writer, "[").unwrap();
+    //let mut spectro = Spectro { data: vec![] };
+    let mut data = Vec::new();
     for (i, s) in samples.enumerate() {
         let sample = s.unwrap();
         buf[j] = sample as f64;
@@ -43,10 +46,11 @@ fn main() {
             if i > 4096 {
                 let harm = harm.unwrap();
                 let col = stft.hpss_one(harm);
-                println!("{}", col.len());
-                write!(writer, "[").unwrap();
-                col.iter().for_each(|c| write!(writer, "{c},").unwrap());
-                writeln!(writer, "],").unwrap();
+                //println!("{}", col.len());
+                //write!(writer, "[").unwrap();
+                data.push(col);
+                //col.iter().for_each(|c| write!(writer, "{c},").unwrap());
+                //writeln!(writer, "],").unwrap();
                 f += 1;
             }
             if f >= args.frames {
@@ -54,5 +58,6 @@ fn main() {
             }
         }
     }
-    write!(writer, "]").unwrap();
+    serde_json::to_writer(writer, &data).unwrap();
+    //write!(writer, "]").unwrap();
 }
--- src/spectrogram.rs
+++ src/spectrogram.rs
@@ -18,14 +18,14 @@ fn new_hann_window(size: usize) -> Vec<f64> {
 
     for n in 0..size {
         let x = n as f64 / (size - 1) as f64;
-        let value = f64::from(0.5 * (1.0 - (2.0 * PI * x).cos()));
+        let value = 0.5 * (1.0 - (2.0 * PI * x).cos());
         window.push(value);
     }
 
     window
 }
 
-pub struct STFT {
+pub struct Stft {
     n_fft: usize,
     hop_length: usize,
     window: Vec<f64>,
@@ -37,7 +37,7 @@ pub struct STFT {
     columns: CircularBuffer<COLS, Vec<Complex<f64>>>,
 }
 
-impl STFT {
+impl Stft {
     pub fn new(n_fft: usize, hop_length: usize) -> Self {
         let mut planner = RealFftPlanner::new();
         let forward = planner.plan_fft_forward(n_fft);
@@ -56,10 +56,6 @@ impl STFT {
         }
     }
 
-    pub fn append_samples(&mut self, input: &[f64]) {
-        self.sample_ring.push_many_back(input);
-    }
-
     pub fn contains_enough_to_compute(&self) -> bool {
         self.n_fft <= self.sample_ring.len()
     }
@@ -76,7 +72,7 @@ impl STFT {
             let mut filter = Filter::new(FILTER_WIDTH);
             let mut filtered = Vec::new();
             col.iter().for_each(|&s| {
-                filtered.push(filter.consume(s.re));
+                filtered.push(filter.consume(s.norm()));
             });
             //self.filtered_columns.push_back(filtered);
             self.columns.push_back(col);
@@ -108,7 +104,7 @@ impl STFT {
                 let mut filtered = Vec::new();
                 let mut filter = Filter::new(FILTER_WIDTH);
                 row.for_each(|&s| {
-                    filtered.push(filter.consume(s.re));
+                    filtered.push(filter.consume(s.norm()));
                 });
                 perc.push(*filtered.last().unwrap());
             }
@@ -147,151 +143,3 @@ impl STFT {
         mask
     }
 }
-
-//pub struct Spectro {
-//    stft: STFT<f64>,
-//    columns: CircularBuffer<COLS, Vec<f64>>,
-//    //filtered_columns: CircularBuffer<COLS, Vec<f64>>,
-//}
-//
-//impl Spectro {
-//    pub fn new() -> Self {
-//        Self {
-//            stft: STFT::new(WINDOW_SIZE, STEP_SIZE).unwrap(),
-//            columns: CircularBuffer::new(),
-//            //filtered_columns: CircularBuffer::new(),
-//        }
-//    }
-//
-//    pub fn process_samples(&mut self, samples: &[f64]) -> Option<Vec<f64>> {
-//        self.stft.append_samples(samples);
-//        let mut res = None;
-//        while self.stft.contains_enough_to_compute() {
-//            let col = self.stft.compute_column().unwrap();
-//
-//            let mut filter = Filter::new(FILTER_WIDTH);
-//            let mut filtered = Vec::new();
-//            col.iter().for_each(|&s| {
-//                filtered.push(filter.consume(s));
-//            });
-//            //self.filtered_columns.push_back(filtered);
-//            res = Some(filtered);
-//            self.columns.push_back(col);
-//
-//            self.stft.move_to_next_column();
-//        }
-//        res
-//    }
-//
-//    pub fn hpss_last(&mut self, mut harm: Vec<f64>) -> Vec<f64> {
-//        //let mut harm = self.filtered_columns.back().unwrap();
-//
-//        let perc = {
-//            let mut perc = Vec::new();
-//            let iter =
-//                (0..ROWS).map(|row_idx| self.columns.iter().flatten().skip(row_idx).step_by(COLS));
-//            for row in iter {
-//                let mut filtered = Vec::new();
-//                let mut filter = Filter::new(FILTER_WIDTH);
-//                row.for_each(|&s| {
-//                    filtered.push(filter.consume(s));
-//                });
-//                perc.push(*filtered.last().unwrap());
-//            }
-//            perc
-//        };
-//
-//        let mask = Self::softmask_one(&harm, &perc);
-//        for (h, m) in harm.iter_mut().zip(mask) {
-//            *h *= m
-//        };
-//        harm
-//    }
-//
-//    //pub fn hpss_full(&mut self) -> Vec<Vec<f64>> {
-//    //    let mut harm = self.filtered_columns.to_vec();
-//    //
-//    //    let perc = {
-//    //        let mut perc = Vec::new();
-//    //        let iter =
-//    //            (0..ROWS).map(|row_idx| self.columns.iter().flatten().skip(row_idx).step_by(COLS));
-//    //        for row in iter {
-//    //            let mut filtered = Vec::new();
-//    //            let mut filter = Filter::new(FILTER_WIDTH);
-//    //            row.for_each(|&s| {
-//    //                filtered.push(filter.consume(s));
-//    //            });
-//    //            perc.push(filtered);
-//    //        }
-//    //        perc
-//    //    };
-//    //
-//    //    let mask = Self::softmask(&harm, &perc);
-//    //    for (hh, mm) in harm.iter_mut().zip(mask) {
-//    //        for (h, m) in hh.iter_mut().zip(mm) {
-//    //            *h *= m;
-//    //        }
-//    //    }
-//    //    harm
-//    //}
-//
-//    fn softmask_one(x: &[f64], y: &[f64]) -> Vec<f64> {
-//        let z = x.iter().zip(y).map(|(x, y)| {
-//            let maxi = x.max(*y);
-//            if maxi < f64::MIN_POSITIVE  {
-//                (1.0, false)
-//            } else {
-//                (maxi, true)
-//            }
-//        });
-//
-//        let mut mask = x.iter().zip(z.clone()).map(|(x, z)| (x / z.0).powi(2)).collect::<Vec<f64>>();
-//
-//        let ref_mask = y.iter().zip(z.clone()).map(|(y, z)| (y / z.0).powi(2));
-//
-//        for ((m, r), z) in (mask.iter_mut().zip(ref_mask)).zip(z) {
-//            if z.1 {
-//                *m /= *m + r
-//            } else {
-//                *m = 0.5
-//            }
-//        }
-//
-//        mask
-//    }
-//
-//    //fn softmask(x: &[Vec<f64>], y: &[Vec<f64>]) -> Vec<Vec<f64>> {
-//    //    let z = x.iter().zip(y).map(|(xx, yy)| {
-//    //        xx.iter().zip(yy).map(|(x, y)| {
-//    //            let maxi = x.max(*y);
-//    //            if maxi < f64::MIN_POSITIVE {
-//    //                (1.0, false)
-//    //            } else {
-//    //                (maxi, true)
-//    //            }
-//    //        })
-//    //    });
-//    //
-//    //    let mut mask = x
-//    //        .iter()
-//    //        .zip(z.clone())
-//    //        .map(|(xx, zz)| xx.iter().zip(zz).map(|(x, z)| (x / z.0).powi(2)).collect::<Vec<f64>>()).collect::<Vec<Vec<f64>>>();
-//    //
-//    //    let ref_mask = y
-//    //        .iter()
-//    //        .zip(z.clone())
-//    //        .map(|(yy, zz)| yy.iter().zip(zz).map(|(y, z)| (y / z.0).powi(2)));
-//    //
-//    //    for ((mm, rr), zz) in (mask.iter_mut().zip(ref_mask)).zip(z) {
-//    //        for ((m, r), z) in (mm.iter_mut().zip(rr)).zip(zz) {
-//    //            if z.1 {
-//    //                *m /= *m + r
-//    //            } else {
-//    //                *m = 0.5
-//    //            }
-//    //        }
-//    //    }
-//    //
-//    //    mask
-//    //}
-//}
