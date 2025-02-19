adf01fa29c9480d7ad1908d9e9d9fe589f14792d
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-02-19 14:49:02 +0100

    x

--- src/main.rs
+++ src/main.rs
@@ -189,7 +189,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             for s in samples {
                 let sample = s.unwrap();
                 if let Some(mut col) = stft.process_samples(&[sample as f64]) {
-                    Stft::hpss_one(&mut col, &stft.harm, &stft.perc);
+                    //Stft::hpss_one(&mut col, &stft.harm, &stft.perc);
                     amplitude_to_db(&mut col);
                     assert_eq!(
                         col.len(),
