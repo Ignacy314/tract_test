d6d4954d658abc016c46458340130608b76ff5f4
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-03-12 16:30:19 +0100

    x

--- spectrogram/src/lib.rs
+++ spectrogram/src/lib.rs
@@ -8,7 +8,7 @@ use realfft::num_complex::Complex;
 use realfft::{RealFftPlanner, RealToComplex};
 use strider::{SliceRing, SliceRingImpl};
 
-pub const N_FFT: usize = 32768;
+pub const N_FFT: usize = 8192;
 pub const HOP_LENGTH: usize = 429;
 pub const FILTER_WIDTH: usize = 33;
 pub const HALF_FILTER_WIDTH: usize = FILTER_WIDTH / 2;
